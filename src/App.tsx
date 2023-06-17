f3c9ebb509521f2d97059d6e76bbd094a56f0ba4
夜夜三倍速 <iwctwbh@gmail.com>
2023-06-17 21:51:37 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./logo.svg";
+import {Button, DatePicker} from 'antd';
 import "./App.css";
 
-function App() {
+function myCard(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>My Card</h1>
+      </div>
+  );
+}
+
+let arrayItems: Array<string> = new Array<string>();
+
+// function MyPosts(): JSX.Element[] {
+//   const [posts, setPosts] = useState(Array<string>);
+//   fetch("http://10.11.12.18:5231/GetPosts",
+//       {
+//         method: 'Post',
+//         headers: new Headers({
+//           'Content-Type': 'application/json'
+//         }),
+//         body: JSON.stringify({"skip": "0"})
+//       })
+//       .then((response) => response.json())
+//       .then((json) => {
+//         arrayItems = new Array<string>();
+//         json.map((m: { content: string; }) => {
+//           arrayItems.push(m.content)
+//         });
+//         setPosts(arrayItems);
+//       });
+//   return (
+//       posts.map(post => <li key={post}>{post}</li>)
+//   );
+// }
+
+
+function PostsList({posts}: { posts: Array<string> }) {
+  const rows: JSX.Element[] = [];
+
+  posts.forEach((post: string) => {
+    rows.push(
+        <li key={post}></li>
+    );
+  });
+
+  return (
+      <ul>{rows.map(m => <li>m</li>)}</ul>
+  );
+}
+
+function FilterableProductTable({posts}: { posts: Array<string> }) {
+  return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+  );
+}
+
+let postsData: Array<string> = new Array<string>();
+
+fetch("http://10.11.12.18:5231/GetPosts",
+    {
+      method: 'Post',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({"skip": "0"})
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      arrayItems = new Array<string>();
+      json.map((m: { content: string; }) => {
+        arrayItems.push(m.content)
+      });
+      postsData = arrayItems;
+    });
+
+function App(): JSX.Element {
+  return (
+      <div className="App">
+        <FilterableProductTable posts={postsData} />
+      </div>
   );
 }
 
