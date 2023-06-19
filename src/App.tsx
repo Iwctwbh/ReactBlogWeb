5d92d901919335f024bb0b90aaf1088c2358981e
夜夜三倍速 <iwctwbh@gmail.com>
2023-06-19 15:04:09 +0800

    Init TestProject

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,190 @@
 import React, {useState} from "react";
 import logo from "./logo.svg";
-import {Button, DatePicker} from 'antd';
+import {Button, DatePicker} from "antd";
 import "./App.css";
+import {typeProduct} from "src/types/product";
 
-function myCard(): JSX.Element {
+function FilterableProductTable({products}: { products: Array<typeProduct> }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+  const [postsData, setPostsData] = useState("");
+
   return (
-      <div>
-        <h1>My Card</h1>
-      </div>
+    <div>
+      <ButtonGetData
+        postsData={postsData}
+        clickEvent={setPostsData}
+      />
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+      <ListPosts
+        postsData={postsData}
+      />
+    </div>
   );
 }
 
-let arrayItems: Array<string> = new Array<string>();
+function ProductCategoryRow({category}: { category: string }): JSX.Element {
+  return (
+    <tr>
+      <th colSpan={2}>{category}</th>
+    </tr>
+  );
+}
 
-// function MyPosts(): JSX.Element[] {
-//   const [posts, setPosts] = useState(Array<string>);
-//   fetch("http://10.11.12.18:5231/GetPosts",
-//       {
-//         method: 'Post',
-//         headers: new Headers({
-//           'Content-Type': 'application/json'
-//         }),
-//         body: JSON.stringify({"skip": "0"})
-//       })
-//       .then((response) => response.json())
-//       .then((json) => {
-//         arrayItems = new Array<string>();
-//         json.map((m: { content: string; }) => {
-//           arrayItems.push(m.content)
-//         });
-//         setPosts(arrayItems);
-//       });
-//   return (
-//       posts.map(post => <li key={post}>{post}</li>)
-//   );
-// }
+function ProductRow({product}: { product: typeProduct }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{color: "red"}}>{product.name}</span>
+  );
 
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
 
-function PostsList({posts}: { posts: Array<string> }) {
-  const rows: JSX.Element[] = [];
+function ProductTable({products, filterText, inStockOnly}: {
+  products: Array<typeProduct>,
+  filterText: string,
+  inStockOnly: boolean
+}) {
+  console.log("logProductTable");
+  const rows: Array<JSX.Element> = [];
+  let lastCategory: string | null = null;
 
-  posts.forEach((post: string) => {
-    rows.push(
-        <li key={post}></li>
-    );
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
   });
 
   return (
-      <ul>{rows.map(m => <li>m</li>)}</ul>
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
   );
 }
 
-function FilterableProductTable({posts}: { posts: Array<string> }) {
+function SearchBar({inStockOnly, onFilterTextChange, onInStockOnlyChange, filterText}: {
+  inStockOnly: boolean,
+  onFilterTextChange: (text: string) => void,
+  onInStockOnlyChange: (checked: boolean) => void,
+  filterText: string
+}): JSX.Element {
+  console.log("logSearchBar");
   return (
-      <div>
-        <PostsList posts={posts} />
-      </div>
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+      <label>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
+        />{" "}
+        Only show products in stock
+      </label>
+    </form>
   );
 }
 
-let postsData: Array<string> = new Array<string>();
+function ButtonGetData({clickEvent, postsData}: {
+  clickEvent: (text: string) => void,
+  postsData: string
+}): JSX.Element {
+  console.log("logButtonGetData");
+  return (
+    <div>
+      <input
+        value={postsData}
+        onChange={(e) => clickEvent(e.target.value)}
+      />
+      <Button
+        type="primary"
+        onClick={
+          () => {
+            fetch("http://10.11.12.18:5231/GetPosts",
+              {
+                method: "Post",
+                headers: new Headers({
+                  "Content-Type": "application/json"
+                }),
+                body: JSON.stringify({"skip": "0"})
+              })
+              .then((response) => response.json())
+              .then((json) => {
+                json.map((m: { content: string; }) => {
+                });
+                clickEvent(json[0].content);
+              });
+          }
+        }
+      >
+        Primary Button
+      </Button>
+    </div>
+  );
+}
 
-fetch("http://10.11.12.18:5231/GetPosts",
-    {
-      method: 'Post',
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      }),
-      body: JSON.stringify({"skip": "0"})
-    })
-    .then((response) => response.json())
-    .then((json) => {
-      arrayItems = new Array<string>();
-      json.map((m: { content: string; }) => {
-        arrayItems.push(m.content)
-      });
-      postsData = arrayItems;
-    });
+function ListPosts({postsData}: {
+  postsData: string
+}): JSX.Element {
+  console.log("logListPosts");
+  return (
+    <li>{postsData}</li>
+  );
+}
+
+const PRODUCTS: Array<typeProduct> = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"},
+];
 
 function App(): JSX.Element {
   return (
-      <div className="App">
-        <FilterableProductTable posts={postsData} />
-      </div>
+    <div className="App">
+      <FilterableProductTable products={PRODUCTS} />
+    </div>
   );
 }
 
---
+++ src/types/product.d.ts
@@ -0,0 +1,3 @@
+declare module 'src/types/product' {
+  export type typeProduct = { name: string, stocked: boolean, category: string, price: string };
+}
