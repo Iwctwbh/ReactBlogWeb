f4465843a888a649db996cd48e2334a8023623aa
夜夜三倍速 <iwctwbh@gmail.com>
2023-06-30 13:55:59 +0800

    Init Project To Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,53 @@
-import React, {useState} from "react";
-import logo from "./logo.svg";
-import {Button, DatePicker} from "antd";
+import React, {ReactElement, useState} from "react";
+import {Card, Space} from "antd";
 import "./App.css";
-import {blogPost} from "src/types/blogPost";
+import {blogPost, blogPosts} from "src/types/blogPost";
+import MDEditor, {commands} from "@uiw/react-md-editor";
 
-function UlPostsData(): JSX.Element {
-  const [postsData, setPostsData] = useState(Array<blogPost>);
+function UlPostsData(): ReactElement {
+  const [postsData, setPostsData] = useState<blogPosts>();
 
   return (
     <div>
-      <ButtonGetData
-        postsData={postsData}
-        clickEvent={setPostsData}
-      />
       <ListPosts
         postsData={postsData}
+        clickEvent={setPostsData}
       />
     </div>
   );
 }
 
-
-function ButtonGetData({clickEvent, postsData}: {
-  clickEvent: (textData: Array<blogPost>) => void,
-  postsData: Array<blogPost>
-}): JSX.Element {
-  console.log("logButtonGetData");
+function ListPosts({clickEvent, postsData}: {
+  clickEvent: (textData: blogPosts) => void,
+  postsData: blogPosts | undefined
+}): ReactElement {
+  if (postsData === undefined) {
+    fetch("http://10.11.12.30:5232/GetPosts",
+      {
+        method: "Post",
+        headers: new Headers({
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({skip: "114", take: "10", order: "asc", search: ""})
+      })
+      .then((response) => response.json())
+      .then((jsonResult: blogPosts) => clickEvent(jsonResult));
+  }
   return (
-    <Button
-      type="primary"
-      onClick={
-        () => {
-          fetch("http://10.11.12.18:5231/GetPosts",
-            {
-              method: "Post",
-              headers: new Headers({
-                "Content-Type": "application/json"
-              }),
-              body: JSON.stringify({"skip": "0"})
-            })
-            .then((response) => response.json())
-            .then((jsonResult: Array<blogPost>) => clickEvent(jsonResult));
-        }
-      }
-    >
-      Primary Button
-    </Button>
+    <Space direction="vertical" size={16}>
+      {postsData?.data.map((m: blogPost) => {
+        return (
+          <Card key={m.Id} title={m.Title} extra={<a href="#">More</a>} style={{width: 500}}>
+            <p>Id:{m.Id} Title{m.Title}</p>
+            <MDEditor.Markdown source={m.Content} />
+          </Card>
+        );
+      })}
+    </Space>
   );
 }
 
-function ListPosts({postsData}: {
-  postsData: Array<blogPost>
-}): JSX.Element {
-  console.log("logListPosts");
-  return (
-    <ul>
-      {postsData.map((m: blogPost) => <li key={m.Id}>Id:{m.Id} Title{m.Title} Content{m.Content}</li>)}
-    </ul>
-  );
-}
-
-function App(): JSX.Element {
+function App(): ReactElement {
   return (
     <div className="App">
       <UlPostsData />
