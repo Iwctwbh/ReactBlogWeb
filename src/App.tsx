9dea149736cb04d005bc5d6a38cdb81fc8b84f3e
夜夜三倍速 <iwctwbh@gmail.com>
2023-07-03 14:04:37 +0800

    Add skip take search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 /** @jsxImportSource @emotion/react */
 import React, {ReactElement, useState} from "react";
-import {Card, Space} from "antd";
+import {Button, Card, InputNumber, Space} from "antd";
 import "./App.css";
 import {blogPost, blogPosts} from "src/types/blogPost";
 import MDEditor from "@uiw/react-md-editor";
 import {PlusOutlined, MinusOutlined} from "@ant-design/icons";
 import {css, SerializedStyles} from "@emotion/react";
 
-
 // element
-function UlPostsData(): ReactElement {
+const PostsCards: React.FC = () => {
   const [postsData, setPostsData] = useState<blogPosts>();
+  const [skip, setSkip] = useState<number>(0);
+  const [take, setTake] = useState<number>(10);
+  const [needRefreshData, setNeedRefreshData] = useState<boolean>(true);
 
   return (
-    <div>
-      <ListPosts
-        postsData={postsData}
-        clickEvent={setPostsData}
-      />
-    </div>
+    <>
+      <Space size={16}>
+        skip:
+        <InputNumber defaultValue={skip} onChange={(v: number | null) => setSkip(v ?? 0)} />
+        take:
+        <InputNumber defaultValue={take} onChange={(v: number | null) => setTake(v ?? 10)} />
+        <Button onClick={() => setNeedRefreshData(true)}>
+          Get Data
+        </Button>
+      </Space>
+      <div>
+        <ListPosts
+          postsData={postsData}
+          clickEvent={setPostsData}
+          skip={skip}
+          take={take}
+          needRefreshData={needRefreshData}
+          refreshEvent={setNeedRefreshData}
+        />
+      </div>
+    </>
   );
-}
+};
 
 function PostCard({Title, Id, Content}: { Title: string, Id: number, Content: string }): ReactElement {
   const [cardStyle, setCardStyle] = useState<SerializedStyles>(cardMinusStyle);
@@ -56,19 +73,24 @@ function PostCard({Title, Id, Content}: { Title: string, Id: number, Content: st
   );
 }
 
-function ListPosts({clickEvent, postsData}: {
+function ListPosts({clickEvent, postsData, skip, take, needRefreshData, refreshEvent}: {
   clickEvent: (textData: blogPosts) => void,
-  postsData: blogPosts | undefined
+  postsData: blogPosts | undefined,
+  skip: number,
+  take: number,
+  needRefreshData: boolean,
+  refreshEvent: (needRefreshData: boolean) => void
 }): ReactElement {
   // First load
-  if (postsData === undefined) {
+  if (needRefreshData) {
+    refreshEvent(false);
     fetch("http://10.11.12.30:5232/GetPosts",
       {
         method: "Post",
         headers: new Headers({
           "Content-Type": "application/json"
         }),
-        body: JSON.stringify({skip: "0", take: "10", order: "asc", search: ""})
+        body: JSON.stringify({skip: skip, take: take, order: "asc", search: ""})
       })
       .then((response) => response.json())
       .then((jsonResult: blogPosts) => clickEvent(jsonResult));
@@ -88,7 +110,7 @@ function ListPosts({clickEvent, postsData}: {
 function App(): ReactElement {
   return (
     <div className="App">
-      <UlPostsData />
+      <PostsCards />
     </div>
   );
 }
