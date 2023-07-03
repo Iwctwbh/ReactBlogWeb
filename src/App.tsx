f873abcd719c71e35fecc845bcd83651cb17d0f5
夜夜三倍速 <iwctwbh@gmail.com>
2023-07-03 11:48:36 +0800

    Update Card Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+/** @jsxImportSource @emotion/react */
 import React, {ReactElement, useState} from "react";
 import {Card, Space} from "antd";
 import "./App.css";
 import {blogPost, blogPosts} from "src/types/blogPost";
-import MDEditor, {commands} from "@uiw/react-md-editor";
+import MDEditor from "@uiw/react-md-editor";
+import {PlusOutlined, MinusOutlined} from "@ant-design/icons";
+import {css, SerializedStyles} from "@emotion/react";
 
+
+// element
 function UlPostsData(): ReactElement {
   const [postsData, setPostsData] = useState<blogPosts>();
 
@@ -17,10 +22,45 @@ function UlPostsData(): ReactElement {
   );
 }
 
+function PostCard({Title, Id, Content}: { Title: string, Id: number, Content: string }): ReactElement {
+  const [cardStyle, setCardStyle] = useState<SerializedStyles>(cardMinusStyle);
+
+  return (
+    <Card
+      key={Id}
+      title={Title}
+      extra={
+        <a href="#">
+          {
+            cardStyle == cardMinusStyle ?
+              <PlusOutlined
+                css={iconHover}
+                onClick={() => {
+                  setCardStyle(cardPlusStyle);
+                }}
+              />
+              :
+              <MinusOutlined
+                css={iconHover}
+                onClick={() => {
+                  setCardStyle(cardMinusStyle);
+                }}
+              />
+          }
+        </a>
+      }
+      css={cardStyle}
+    >
+      <MDEditor.Markdown source={Content} />
+    </Card>
+  );
+}
+
 function ListPosts({clickEvent, postsData}: {
   clickEvent: (textData: blogPosts) => void,
   postsData: blogPosts | undefined
 }): ReactElement {
+  // First load
   if (postsData === undefined) {
     fetch("http://10.11.12.30:5232/GetPosts",
       {
@@ -28,7 +68,7 @@ function ListPosts({clickEvent, postsData}: {
         headers: new Headers({
           "Content-Type": "application/json"
         }),
-        body: JSON.stringify({skip: "114", take: "10", order: "asc", search: ""})
+        body: JSON.stringify({skip: "0", take: "10", order: "asc", search: ""})
       })
       .then((response) => response.json())
       .then((jsonResult: blogPosts) => clickEvent(jsonResult));
@@ -37,16 +77,14 @@ function ListPosts({clickEvent, postsData}: {
     <Space direction="vertical" size={16}>
       {postsData?.data.map((m: blogPost) => {
         return (
-          <Card key={m.Id} title={m.Title} extra={<a href="#">More</a>} style={{width: 500}}>
-            <p>Id:{m.Id} Title{m.Title}</p>
-            <MDEditor.Markdown source={m.Content} />
-          </Card>
+          <PostCard Id={m.Id} Title={m.Title} Content={m.Content}></PostCard>
         );
       })}
     </Space>
   );
 }
 
+// App
 function App(): ReactElement {
   return (
     <div className="App">
@@ -55,4 +93,26 @@ function App(): ReactElement {
   );
 }
 
+// function
+
+// style
+const iconHover = css`
+  &:hover {
+    color: #252525;
+  }
+`;
+
+const cardMinusStyle = css`
+  max-width: 1000px;
+  height: 200px;
+  overflow: hidden;
+`;
+
+const cardPlusStyle = css`
+  height: 100%;
+  overflow: unset;
+`;
+
+
+// export
 export default App;
