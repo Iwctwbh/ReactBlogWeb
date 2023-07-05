6dc998bd2b57131a4be87cb7b51da4a970ddcea2
夜夜三倍速 <iwctwbh@gmail.com>
2023-07-05 13:31:47 +0800

    Init gh-pages

--- src/types/blogPost.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "src/types/blogPost" {
-  export type blogPost = { Id: number, Title: string, Content: string, DateCreated: Date, DateModified: Date };
-  export type blogPosts = { totalCount: number, data: Array<blogPost> };
-  // export abstract class blogPosts  { totalCount: number; data: Array<blogPost> };
-}
